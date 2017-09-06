0db416cdce4c079bfb2770f0d6089431fddb63b9
Lonnie Souder II <souderl9@students.rowan.edu>
2017-09-06 13:35:51 -0400

    Fix error in math:math()

--- Assignment/math.c
+++ Assignment/math.c
@@ -6,6 +6,6 @@ int math(int num1, int num2, char Operator) {
         case('-') : return num1 - num2;
         case('*') : return num1 * num2;
         case('/') : return num1 / num2;
-        default : return;
+        default : return 0;
     }
 }
